9113cc343905e6de8fe08c578f78406cd572997f
YiLin Ong <chauntelleinc@gmail.com>
2022-05-10 19:32:23 -0400

    get route to signup account

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,3 +41,20 @@ router.get('/', (req, res) => {
     });
 });
 
+// if user clicks on login, the page will be redirected to handlebars page 
+router.get('/login', (req, res) => {
+        if (req.session.loggedIn) {
+            res.redirect('/');
+            return;
+        }
+        res.render('login');
+    });
+
+// if user clicks on signup, user will get the same response as login
+router.get('/signup', (req, res) {
+    res.render('signup');
+});
+
+
+
+
