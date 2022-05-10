f8c9d40f76c0c7cf356dd323ee3122ed47a47326
toma <toma95s@tomas-MBP.lan>
2022-05-10 15:41:15 -0700

    added .init which initializes the model.

---
+++ models/Post.js
@@ -0,0 +1,32 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+class Post extends Model {}
+
+
+//.init initializes the model.
+Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      }
+    },
+    {
