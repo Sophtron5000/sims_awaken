19604f84bfb942a89ce876eed36fcc4a4e13902c
YiLin Ong <chauntelleinc@gmail.com>
2022-05-10 14:07:20 -0400

    set up connections

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-//dependencies 
-// set up path which is uses for handling and transforming file paths.
+//dependencies.
+//set up path which is uses for handling and transforming file paths.
 const path = require('path');
 
 //set up express so we can handle multiple http requests at specific urls
@@ -34,7 +34,7 @@ const hdb = exphbs.create({ helpers });
 
 const sess = {
     // required to sign the session ID cookie
-    secret: '',
+    secret: 'Choose the red pill or blue pill',
 
     //session ID cookie
     cookie: { maxAge: null},
@@ -45,15 +45,24 @@ const sess = {
     saveUninitialized: true, 
 
     store: new SequelizeStore({
-        //connect sequelize with express and session to db
+//connect sequelize with express and session to db
     })
 };
 //creates middleware for session
 app.use(session(sess));
 
+// tells express.js which template engine to utilize
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on PORT: ${PORT}!`));
+});
 
 
-
-
-
-}
