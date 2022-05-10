2758bdeaeb4783fbf69f9fd4f2fdd0b02a17980f
YiLin Ong <chauntelleinc@gmail.com>
2022-05-10 17:48:13 -0400

    indent problems fixed by asika

--- models/index.js
+++ models/index.js
@@ -6,33 +6,33 @@ const Comment = require('./Comment');
 // foreign key to target plural association mixins to the source.
 
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+  foreignKey: 'user_id'
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: "cascade"
+  foreignKey: 'user_id',
+  onDelete: 'cascade'
 });
 
 Post.hasMany(Comment, {
-    foreignKey: 'post_id',
-    onDelete: "cascade"
+  foreignKey: 'post_id',
+  onDelete: 'cascade'
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: "cascade"
+  foreignKey: 'user_id',
+  onDelete: 'cascade'
 });
 
 User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: "cascade"
+  foreignKey: 'user_id',
+  onDelete: 'cascade'
 });
 
 Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
-    onDelete: "cascade" 
-})
+  foreignKey: 'post_id',
+  onDelete: 'cascade'
+});
 
 module.exports = { User, Post, Comment };
 
