36a3a47c59ff13075130a37d7ab41e3b08e5f3de
toma <toma95s@tomas-MBP.lan>
2022-05-10 14:55:58 -0700

    finished comment model with post id and all

--- models/Comment.js
+++ models/Comment.js
@@ -8,23 +8,40 @@ const { Model, DataTypes } = require('sequelize');
 class Comment extends Model {}
 
 Comment.init({
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    comment_text: {
-        type: DataTypes.STRING,
-        validate: {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  comment_text: {
+    type: DataTypes.STRING,
+    validate: {
 
-            len: [3]
-        }
-    },
-    user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'user',
-            key: 'id'
-        }
-    },
+      len: [3]
+    }
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'user',
+      key: 'id'
+    }
+  },
+  post_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'post',
+      key: 'id'
+    }
+  }
+}, {
+  sequelize,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'comment'
+});
+
+
+module.exports = Comment;
