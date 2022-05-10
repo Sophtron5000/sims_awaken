debd168adfabc9c3bcd69aa48f1fd7b208bd22b3
YiLin Ong <chauntelleinc@gmail.com>
2022-05-10 19:14:44 -0400

    get route to edit an id and get new post

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
@@ -42,3 +43,52 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+// when user clicks to edit an id, they have to be logged in
+router.get('/edit/:id', withAuth, (req, res) => {
+    // find the post by id 
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [{
+            model: User,
+            attributes: ['id', 'comment_text', 'post_id', 'created_at'],
+
+            include: {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+        // if there is no post found by this id, post error
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({ message: 'No post found with this id. ' });
+                return;
+
+            }
+            const post = dbPostData.get({ plain: true });
+            res.render('edit-post', { post, loggedIn: true });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// when a user clicks /new to add a post, render that page
+router.get('/new', (req, res) => {
+    res.render('add-post');
+});
+
+module.exports = router;
