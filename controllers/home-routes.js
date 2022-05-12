8a26879b7439790d3231a2a940f882d864005fa0
YiLin Ong <chauntelleinc@gmail.com>
2022-05-12 12:40:58 -0400

    debugging code on home-routes.js and server.js and post-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,44 +1,15 @@
 // import our connections
+// eslint-disable-next-line no-unused-vars
 const sequelize = require('../config/connection');
 // import our Post, User, Comment models
+// eslint-disable-next-line no-unused-vars
 const { Post, User, Comment } = require('../models');
 // import express router
 const router = require('express').Router();
 
 // get route to homepage, and find all posts
 router.get('/', (req, res) => {
-  // read the entire table from db with findAll()
-  Post.findAll({
-    attributes: [
-      'id',
-      'title',
-      'content',
-      'created_at'
-    ],
-    include: [{
-      model: Comment,
-      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-      include: {
-        model: User,
-        attributes: ['username']
-      }
-    },
-    {
-      model: User,
-      attributes: ['username']
-    }
-    ]
-  })
-    .then(dbPostData => {
-      const posts = dbPostData.map(post => post.get({ plain: true }));
-      console.log('===================');
-      console.log('===================');
-      res.render('homepage', { posts, loggedIn: req.session.loggedIn });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
+  res.render('homepage');
 });
 
 // if user clicks on login, the page will be redirected to handlebars page 
@@ -55,50 +26,7 @@ router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
-// user can view the single post when click on the post on the homepage
-// user can also add comments
-router.get('/post/:id', (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'content',
-      'title',
-      'created_at'
-    ],
-    include: [{
-      model: Comment,
-      attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            
-      include: {
-        model: User,
-        attributes: ['username']
-      }
-    },
-    {
-      model: User,
-      attributes: ['username']
-    }
-    ]
-  })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-      const post = dbPostData.get({ plain: true });
-      console.log(post);
-      res.render('single-post', { post, loggedIn: req.session.loggedIn });
 
-
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
 
 module.exports = router;
 
